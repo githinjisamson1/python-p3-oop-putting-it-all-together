c9501e44bf2b9003bd9a7e61ec61c8cc7edce5f1
githinjisamson1 <githinjisamson148@gmail.com>
2023-12-06 04:40:00 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self._title = title
+
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+
+    # getters
+    def get_page_count(self):
+        return self._page_count
+
+    def get_title(self):
+        return self._title
+
+    # setters
+    def set_page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+
+    def set_title(self, title):
+        self._title = title
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    page_count = property(get_page_count, set_page_count)
+    title = property(get_title, set_title)
+
+
+book1 = Book("The Pearl", 200)
+book1.turn_page()
+# print(book1)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,38 @@
-#!/usr/bin/env python3
+
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+
+        if not isinstance(size, int):
+            print("size must be an integer")
+        else:
+            self._size = size
+
+        self.condition = "New"
+
+    def get_brand(self):
+        return self._brand
+
+    def get_size(self):
+        return self._size
+
+    def set_brand(self, brand):
+        self._brand = brand
+
+    def set_size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+        else:
+
+            self._size = size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+
+    brand = property(get_brand, set_brand)
+    size = property(get_size, set_size)
+
+
+shoe1 = Shoe("Bata", 36)
+shoe1.condition = "New"
